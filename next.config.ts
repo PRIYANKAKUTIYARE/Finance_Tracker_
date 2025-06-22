c2c10e113dadbf3f74aa929d46e8d2b3a762480c
PRIYANKAKUTIYARE <139627672+PRIYANKAKUTIYARE@users.noreply.github.com>
2025-06-22 23:36:07 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
-
-//
-//
-//
-//
-/*
-
-*/
-
-
 export default nextConfig;
+
+
+
+
+
+
+
